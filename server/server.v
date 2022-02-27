73d09347c2772a30e9087b01fbdaab126ae714a3
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2022-02-27 21:12:58 +0100

    garbage

--- server/server.v
+++ server/server.v
@@ -24,20 +24,28 @@ pub fn run() {
 	for {
 		mut read_buf := []byte{len:1024} // maximum of 1kb per connection/packet to reduce data throughput & load
 		mut conn := server.listener.accept() or { panic('connection terminated') }
-		buf_fill := conn.read(mut read_buf) or { panic('failed to read data') }
+		conn.set_blocking(true) or { panic('failed to set to blocking') }
+
+		buf_fill := conn.read(mut read_buf) or { fuck_() }
 		println(read_buf[0..buf_fill])
 		handle_connection(mut conn, mut read_buf)
-		read_buf.clear()
-		buf_fill2 := conn.read(mut read_buf) or { panic('failed to read data') }
+		// read_buf.clear()
+
+		buf_fill2 := conn.read(mut read_buf) or { fuck_() }
 		println(read_buf[0..buf_fill2])
 		handle_connection(mut conn, mut read_buf)
-		read_buf.clear()
-		buf_fill3 := conn.read(mut read_buf) or { panic('failed to read data') }
+		// read_buf.clear()
+
+		buf_fill3 := conn.read(mut read_buf) or { fuck_() }
 		println(read_buf[0..buf_fill3])
 		handle_connection(mut conn, mut read_buf)
-		read_buf.clear()
+		// read_buf.clear()
 	}
 
+}
+
+[noreturn]
+fn fuck_() {
 }
 
 fn handle_connection(mut conn net.TcpConn, mut read_buf []byte) {
@@ -45,6 +53,7 @@ fn handle_connection(mut conn net.TcpConn, mut read_buf []byte) {
 	packet := decode_packet(read_buf)
 
 	if packet is CHandshake {
+		println('processing handshake...')
 		process_handshake(mut conn, packet)
 	} if packet is CAuthRequest {
 		println(packet.key)
@@ -59,14 +68,16 @@ fn handle_connection(mut conn net.TcpConn, mut read_buf []byte) {
 
 fn process_handshake(mut conn net.TcpConn, packet CHandshake) {
 	s_handshake := SHandshake{packet.id, packet.ts}
+	println('returning handshake')
 	conn.write(encode_handshake(s_handshake)) or { panic('oh noes') }
+	println('handshake returned')
 }
 
 fn process_auth_request(mut conn net.TcpConn, packet CAuthRequest) {
 	if packet.key == 'blub' {
 		conn.write(encode_clir_request()) or { panic('failed to write clir request') }
 	} else {
-		conn.write(encode_invalid()) or { panic('failed to write invalid packet') }
+		conn.write(encode_invalid()) or { panic('failed to write invalid') }
 	}
 }
 
